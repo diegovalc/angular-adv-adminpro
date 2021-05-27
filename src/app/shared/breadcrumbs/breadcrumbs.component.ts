9f17bff5f9dea0e5a3f285a9d3b442c5bb08ad26
Diego Valdez <diegovaldezca@gmail.com>
2021-05-27 15:24:42 -0600

    Fin Seccion 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,13 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.css']
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public titulo: string;
+  public tituloSub$: Subscription
+
+  constructor( private router: Router) { 
+    this.tituloSub$ = this.getArgumentosRuta().subscribe( data => {
+      console.log(data);
+      this.titulo = data.titulo;
+      document.title = this.titulo;
+    } );
+  }
+  ngOnDestroy(): void {
+    
+    this.tituloSub$.unsubscribe();
+
+  }
+
+  getArgumentosRuta(){
+    return this.router.events
+    .pipe(
+      filter( event => event instanceof ActivationEnd ),
+      filter( (event: ActivationEnd) =>  event.snapshot.firstChild === null ),
+      map( (event: ActivationEnd) =>  event.snapshot.data)
+    );
+    
+  }
 
   ngOnInit(): void {
   }
